d8c25b4366bac8b2472409a01386f990ae4cd593
Yurii Sv <30704362+1Zero11@users.noreply.github.com>
2021-09-23 18:00:53 +0300

    Довожу до ума

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ myprojects = [
     {
         'name': "Foxy: Nightmares",
         'description': "A game I've made with a friend long ago",
+        'imgsrc': "Nightmareslogo.png",
         'href': "https://play.google.com/store/apps/details?id=com.HotKoreetz.DDA&hl=en&gl=US"
     },
     {
         'name': "Shodo Master",
         'description': "My personal half-developed abandoned project",
-        'href': "Some other href"
+        'imgsrc': "shodologo.png",
+        'href': "https://play.google.com/store/apps/details?id=com.Jidake.ShodoMaster&hl=en&gl=US"
     }
 ]
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title = 'Home')
 
 @app.route("/projects")
 def projects():
-    return render_template('projects.html', projects = myprojects)
+    return render_template('projects.html', title = 'Projects', projects = myprojects)
 
 @app.route("/about")
 def about():
